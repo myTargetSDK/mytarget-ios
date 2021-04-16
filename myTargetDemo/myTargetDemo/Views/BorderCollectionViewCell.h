500af80005a5b827d4eb469e9138430d024830da
Andrey Seredkin <a.seredkin@corp.mail.ru>
2021-04-16 16:27:20 +0300

    Removed old demo app

--- myTargetDemo/myTargetDemo/Views/BorderCollectionViewCell.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Anton Bulankin on 05.07.16.
-// Copyright (c) 2016 Mail.ru Group. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-@interface BorderCollectionViewCell : UICollectionViewCell
-@end
